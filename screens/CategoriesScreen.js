7401e22a1ce89a8bac98ff0942a2db475aaa3e84
Balazs Danyadi <balazs.danyadi@gmail.com>
2020-08-31 14:20:39 +0200

    fetchfonts

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,12 +1,20 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
 
 const CategoriesScreen = () => {
-    return ()
+  return (
+    <View style={styles.screen}>
+      <Text>Categories Screen</Text>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-    
-})
+  screen: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
 
 export default CategoriesScreen;
