3dfac0bd42b1bf76f78bc0507c852d31d8065f4f
Balazs Danyadi <balazs.danyadi@gmail.com>
2020-09-01 13:38:47 +0200

    click on item works

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,15 +1,29 @@
 import React from "react";
-import { Text, View, StyleSheet, Button, FlatList } from "react-native";
+import {
+  Text,
+  View,
+  StyleSheet,
+  TouchableOpacity,
+  FlatList,
+} from "react-native";
 import { CATEGORIES } from "../data/dummy-data";
 
-const renderGridItem = (itemData) => {
-  return (
-    <View>
-      <Text>{itemData.item.title}</Text>
-    </View>
-  );
-};
 const CategoriesScreen = (props) => {
+  const renderGridItem = (itemData) => {
+    return (
+      <TouchableOpacity
+        style={styles.gridItem}
+        onPress={() => {
+          props.navigation.navigate({ routeName: "CategoriesMeals" });
+        }}
+      >
+        <View>
+          <Text>{itemData.item.title}</Text>
+        </View>
+      </TouchableOpacity>
+    );
+  };
+
   return (
     <FlatList data={CATEGORIES} renderItem={renderGridItem} numColumns={2} />
   );
@@ -20,6 +34,15 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
+  },
+  gridItem: {
+    flex: 1,
+    margin: 15,
+    height: 150,
+    justifyContent: "center",
+    alignItems: "center",
+    borderWidth: 1,
+    borderColor: "black",
   },
 });
 
