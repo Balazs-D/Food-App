593f4b0e0190afcbed5917eb32cc581203e5e4c9
Balazs Danyadi <balazs.danyadi@gmail.com>
2020-09-01 14:06:13 +0200

    color header

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -7,6 +7,7 @@ import {
   FlatList,
 } from "react-native";
 import { CATEGORIES } from "../data/dummy-data";
+import Colors from "../constants/Colors";
 
 const CategoriesScreen = (props) => {
   const renderGridItem = (itemData) => {
@@ -27,6 +28,12 @@ const CategoriesScreen = (props) => {
   return (
     <FlatList data={CATEGORIES} renderItem={renderGridItem} numColumns={2} />
   );
+};
+CategoriesScreen.navigationOptions = {
+  headerStyle: {
+    backgroundColor: Colors.accentColor,
+  },
+  headerTintColor: Colors.primaryColor,
 };
 
 const styles = StyleSheet.create({
